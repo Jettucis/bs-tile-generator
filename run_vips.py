6a58d3dfe159b61384808cfd6b67bc5f0dac113a
GauChoob <starlight_tonight@hotmail.com>
2024-11-23 13:13:45 -0500

    Re-create blank.png and further project tidying

--- run_vips.py
+++ run_vips.py
@@ -1,23 +1,27 @@
-import math
 import pyvips
 import os
 import shutil
+
 
 def sanitize_directory(directory):
     shutil.rmtree(directory, ignore_errors=True)
     os.makedirs(directory)
 
 
-def parse_map_file(file):
+def parse_map_file(file, background_color):
     map = pyvips.Image.new_from_file(file, access='sequential')
-    map = map.flatten(background=[0, 0, 0])
+    map = map.flatten(background=background_color)
     map.dzsave(
         "out/tiles",
         layout="google",
         suffix=".png[compression=9]",
-        background=[0, 0, 0]
+        background=background_color
     )
+    # In dzsave, the background parameter properly tints tiles, but appears to be somehow bugged for blank.png when a non-monochrome parameter is provided
+    # Therefore, we manually recreate the blank.png file
+    background_color_image = pyvips.Image.new_from_memory(bytes(background_color*256*256), 256, 256, 3, 'uchar')
+    background_color_image.pngsave('out/tiles/blank.png', compression=9)
 
 
 sanitize_directory('out/')
-parse_map_file('all-map-mini-test.png')
+parse_map_file('all-map-mini-test.png', [0, 0, 0])
