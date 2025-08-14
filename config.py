9534e592914a18a30eaa8a088758749eb7de5bde
Jettucis <45320856+Jettucis@users.noreply.github.com>
2025-08-15 00:16:00 +0300

    August 12th update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ MAP_FILE = 'map.png'
 
 # This data should match bs-cartography: https://github.com/GauChoob/bs-cartography/blob/main/src/config.js
 TILE_WIDTH = 48
-HOPEPORT_PORTAL_STONE_IMAGE_X = 4537
+HOPEPORT_PORTAL_STONE_IMAGE_X = 4536
 HOPEPORT_PORTAL_STONE_IMAGE_Y = 3432
 HOPEPORT_PORTAL_STONE_COORD_X = 94
 HOPEPORT_PORTAL_STONE_COORD_Y = 71
